7827f7e3e9975d01ed259ebf7692eb42bef30544
username <email>
2020-11-27 14:46:51 +0800

    feat: 初始化仓库呀

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>App...</p>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// @ts-nocheck
+import Vue from "vue";
+import App from "./App";
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
