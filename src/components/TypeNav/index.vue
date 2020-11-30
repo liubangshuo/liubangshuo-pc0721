4f80cffa9e10147629af7faf3b80b8b72b80eafd
username <email>
2020-11-30 15:02:48 +0800

    跳转

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -1,7 +1,11 @@
 <template>
   <!-- 商品分类导航 -->
   <div class="type-nav">
-    <div class="container">
+    <div
+      class="container"
+      @mouseenter="isSearchShow = true"
+      @mouseleave="isSearchShow = false"
+    >
       <h2 class="all">全部商品分类</h2>
       <nav class="nav">
         <a href="###">服装城</a>
@@ -13,61 +17,184 @@
         <a href="###">有趣</a>
         <a href="###">秒杀</a>
       </nav>
-      <div class="sort">
-        <div class="all-sort-list2">
-          <div
-            class="item bo"
-            v-for="category in categoryList"
-            :key="category.categoryId"
-          >
-            <h3>
-              <!-- 一级分类名称 -->
-              <a href="">{{ category.categoryName }}</a>
-            </h3>
-            <div class="item-list clearfix">
-              <div class="subitem">
-                <dl
-                  class="fore"
-                  v-for="child in category.categoryChild"
-                  :key="child.categoryId"
+      <transition name="search">
+        <div class="sort" v-show="isHomeShow || isSearchShow">
+          <div class="all-sort-list2" @click="goSearch">
+            <div
+              class="item bo"
+              v-for="category in categoryList"
+              :key="category.categoryId"
+            >
+              <h3>
+                <!-- 一级分类名称 -->
+                <a
+                  :data-categoryName="category.categoryName"
+                  :data-categoryId="category.categoryId"
+                  :data-categoryType="1"
+                  >{{ category.categoryName }}</a
                 >
-                  <dt>
-                    <!-- 二级分类名称 -->
-                    <a href="">{{ child.categoryName }}</a>
-                  </dt>
-                  <dd>
-                    <!-- 三级分类名称 -->
-                    <em
-                      v-for="grandChild in child.categoryChild"
-                      :key="grandChild.categoryId"
-                    >
-                      <a href="">{{ grandChild.categoryName }}</a>
-                    </em>
-                  </dd>
-                </dl>
+                <!-- 第一种方案：使用router-link跳转，问题产生太多组件，页面性能不会很好 -->
+                <!-- <router-link
+                :to="`/search?categoryName=${category.categoryName}&category1Id=${category.categoryId}`"
+                >{{ category.categoryName }}</router-link
+              > -->
+                <!-- 第二种方案：编程式导航 -->
+                <!-- <a
+                @click.prevent="
+                  $router.push({
+                    name: 'search',
+                    query: {
+                      categoryName: category.categoryName,
+                      category1Id: category.categoryId,
+                    },
+                  })
+                "
+                >{{ category.categoryName }}</a
+              > -->
+              </h3>
+              <div class="item-list clearfix">
+                <div class="subitem">
+                  <dl
+                    class="fore"
+                    v-for="child in category.categoryChild"
+                    :key="child.categoryId"
+                  >
+                    <dt>
+                      <!-- 二级分类名称 -->
+                      <a
+                        :data-categoryName="child.categoryName"
+                        :data-categoryId="child.categoryId"
+                        :data-categoryType="2"
+                        >{{ child.categoryName }}</a
+                      >
+                      <!-- <router-link
+                      :to="`/search?categoryName=${child.categoryName}&category2Id=${child.categoryId}`"
+                      >{{ child.categoryName }}</router-link
+                    > -->
+                      <!-- <a
+                      @click.prevent="
+                        $router.push({
+                          name: 'search',
+                          query: {
+                            categoryName: child.categoryName,
+                            category2Id: child.categoryId,
+                          },
+                        })
+                      "
+                      >{{ child.categoryName }}</a
+                    > -->
+                    </dt>
+                    <dd>
+                      <!-- 三级分类名称 -->
+                      <em
+                        v-for="grandChild in child.categoryChild"
+                        :key="grandChild.categoryId"
+                      >
+                        <a
+                          :data-categoryName="grandChild.categoryName"
+                          :data-categoryId="grandChild.categoryId"
+                          :data-categoryType="3"
+                          >{{ grandChild.categoryName }}</a
+                        >
+                        <!-- <router-link
+                        :to="`/search?categoryName=${grandChild.categoryName}&category3Id=${grandChild.categoryId}`"
+                        >{{ grandChild.categoryName }}</router-link
+                      > -->
+                        <!-- <a
+                        @click.prevent="
+                          $router.push({
+                            name: 'search',
+                            query: {
+                              categoryName: grandChild.categoryName,
+                              category3Id: grandChild.categoryId,
+                            },
+                          })
+                        "
+                        >{{ grandChild.categoryName }}</a
+                      > -->
+                      </em>
+                    </dd>
+                  </dl>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
-import { reqGetBaseCategoryList } from "@api/home";
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "TypeNav",
   data() {
     return {
       // 初始化响应式数据
-      categoryList: [],
+      isHomeShow: this.$route.path === "/",
+      isSearchShow: false,
     };
   },
-  async mounted() {
-    const result = await reqGetBaseCategoryList();
-    this.categoryList = result.slice(0, 16);
+  computed: {
+    // 当使用vuex模块化，这种方式就不行了
+    // ...mapState(["testCount", "categoryList"]),
+    // ...mapState(["testCount", "home.categoryList"]),
+    // ...mapState(["testCount", "home"]), // 这种方式可以，需要使用 this.home.categoryList。太麻烦
+
+    ...mapState({
+      // 对象中的数据，就会传递给组件
+
+      // categoryList就是组件能接受到的数据
+      // 它的值是一个函数，函数内部会调用得到值
+      // 调用时会将所有vuex数据传递进去，就是state
+      categoryList: (state) => state.home.categoryList,
+      // testCount: (state) => state.testCount,
+    }),
+  },
+  methods: {
+    // 函数直接写
+    // 注意：将来action函数名称和mutation函数名称不要重复
+    ...mapActions(["getCategoryList"]),
+    // 跳转到search
+    goSearch(e) {
+      const { categoryname, categoryid, categorytype } = e.target.dataset; // 元素自定义属性对象
+
+      // 需求：如何获取需要的参数？
+      // 已知：得到触发事件目标元素
+      // 解决：给元素设置自定义属性 data-xxx, 通过自定义属性得到需要的参数
+
+      // 判断是否是点中了a标签，才能跳转
+      if (!categoryname) return;
+
+      // 隐藏分类列表
+      this.isSearchShow = false;
+
+      const location = {
+        name: "search",
+        query: {
+          categoryName: categoryname,
+          [`category${categorytype}Id`]: categoryid,
+        },
+      };
+
+      // 判断当前是否有params参数，有加上
+      const { searchText } = this.$route.params;
+
+      if (searchText) {
+        location.params = {
+          searchText,
+        };
+      }
+
+      this.$router.push(location);
+    },
+  },
+  mounted() {
+    // console.log(this);
+    // 调用vuex的action函数
+    this.getCategoryList();
   },
 };
 </script>
@@ -112,6 +239,14 @@ export default {
       position: absolute;
       background: #fafafa;
       z-index: 999;
+      // 控制显示隐藏过渡效果
+      &.search-enter-active {
+        transition: height 0.5s;
+        overflow: hidden;
+      }
+      &.search-enter {
+        height: 0px;
+      }
 
       .all-sort-list2 {
         .item {
--- src/main.js
+++ src/main.js
@@ -3,17 +3,18 @@ import Vue from "vue";
 import App from "./App";
 
 import router from "./router";
+import store from "./store";
+
+import "./plugins/element.js";
 
 // 引入公共资源
 import "./styles/reset.css";
 
-import './plugins/element.js'
-
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
   render: (h) => h(App),
   // 应用router
   router,
+  store,
 }).$mount("#app");
